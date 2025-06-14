74825f3c464bb3f45bf2beee00d4efa4124b9938
saurabh lanjewar <saurabhlanjewar1111@gmail.com>
2025-06-14 14:51:49 +0530

    frontend code

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CodeReviewTool from './components/CodeReviewTool';
+
+function App() {
+  return (
+    <div className="App">
+      <CodeReviewTool />
+    </div>
+  );
+}
+
+export default App;
